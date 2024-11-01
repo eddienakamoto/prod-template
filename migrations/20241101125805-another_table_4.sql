fd2fbf4b62b714bda200e4d47ffe0caf014e7a7f
Eddie Ciccone <eddie@nakamotomining.ai>
2024-11-01 13:07:33 -0500

    reverted to commit 048f3f86189712c7ac7b1153f0da1a189dc5b527

--- migrations/20241101125805-another_table_4.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-CREATE TABLE another (
-    id SERIAL PRIMARY KEY
-);
-
--- +migrate Down
-DROP TABLE another;
--- migrationsx/20241101130207-another_table_5.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-CREATE TABLE anotherr (
-    id SERIAL PRIMARY KEY
-);
-
--- +migrate Down
-DROP TABLE anotherr;
